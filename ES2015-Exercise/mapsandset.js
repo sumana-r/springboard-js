9badc96f9dbc108390788261484850e5090f5104
sumana ramamoorthy <sumanamaninesan@gmail.com>
2022-11-07 19:24:47 -0500

    map and set exercise

--- ES2015-Exercise/mapsandset.js
+++ ES2015-Exercise/mapsandset.js
@@ -2,7 +2,7 @@
 //Quick Question #1
 //What does the following code return?
 
-new Set([1,1,2,2,3,4]) // {1,2,3,4}
+new Set([1, 1, 2, 2, 3, 4]) // {1,2,3,4}
 //Quick Question #2
 //What does the following code return?
 
@@ -11,8 +11,8 @@ new Set([1,1,2,2,3,4]) // {1,2,3,4}
 //What does the Map m look like after running the following code?
 
 let m = new Map();
-m.set([1,2,3], true);
-m.set([1,2,3], false); // [{[1,2,3]: true},{[1,2,3]: false}
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false); // [{[1,2,3]: true},{[1,2,3]: false}
 
 //hasDuplicate
 //Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
@@ -20,13 +20,13 @@ m.set([1,2,3], false); // [{[1,2,3]: true},{[1,2,3]: false}
 //hasDuplicate([1,3,2,1]) // true
 //hasDuplicate([1,5,-1,4]) // false
 
-function hasDuplicate(arr){
+function hasDuplicate(arr) {
     const setArr = new Set(arr);
     let result = false;
-    if(arr.length !== setArr.size){
+    if (arr.length !== setArr.size) {
         result = true;
-     }
-     return result;
+    }
+    return result;
 }
 
 //vowelCount
@@ -34,15 +34,15 @@ function hasDuplicate(arr){
 
 //vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
 //vowelCount('Colt') // Map { 'o' => 1 }
-function vowelCount(val){
-   
-   const vowels = new Set('aeiou');
-  return val.split("").reduce((accum,nextval) => {
-            if(vowels.has(nextval)){
-             const getval = (accum.get(nextval) || 0) + 1;
-              accum.set(nextval, getval);
-            }
-            return accum;
-   },new Map());
+function vowelCount(val) {
+
+    const vowels = new Set('aeiou');
+    return val.split("").reduce((accum, nextval) => {
+        if (vowels.has(nextval)) {
+            const getval = (accum.get(nextval) || 0) + 1;
+            accum.set(nextval, getval);
+        }
+        return accum;
+    }, new Map());
 
 }
